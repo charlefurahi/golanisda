d271fa76a51a55fcf0429a7ce3e119e1d092e839
charlefurahi <charlefurahi@gmail.com>
2026-01-31 12:53:31 +0300

    Import HeroSection component in Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script setup>
+import HeroSection from '../components/home/HeroSection.vue'
 import AnnouncementsPreview from '../components/home/AnnouncementsPreview.vue'
 import WorshipSchedule from '../components/home/WorshipSchedule.vue'
 import MinistriesPreview from '../components/home/MinistriesPreview.vue'
